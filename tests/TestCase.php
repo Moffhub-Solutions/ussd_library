605833a956e84c52356a1a557b572ac7362a8b4c
Moffat Munene <moffmu@gmail.com>
2025-08-10 13:10:02 +0300

    USSD: set up test

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Moffhub\Ussd\Tests;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use UssdServiceProvider;
+
+
+abstract class TestCase extends BaseTestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            UssdServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        //
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Moffhub\Ussd\Tests\Unit;
+
+use Moffhub\Ussd\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function exampleTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
