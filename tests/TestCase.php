5fdeb42fe3813dca09e828bc8f8c68d50b3cff97
Moffat Munene <moffmu@gmail.com>
2025-08-10 13:10:58 +0300

    USSD: lint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 namespace Moffhub\Ussd\Tests;
+
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use UssdServiceProvider;
-
 
 abstract class TestCase extends BaseTestCase
 {
