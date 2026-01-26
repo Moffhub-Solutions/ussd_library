69d9fa4008661c216a769bdf8f5d169085514741
Moffat Munene <moffmu@gmail.com>
2026-01-26 19:13:33 +0300

    test fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,6 +18,20 @@ abstract class TestCase extends BaseTestCase
 
     protected function getEnvironmentSetUp($app): void
     {
-        //
+        // Use SQLite in-memory database for testing
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        // Configure cache to use array driver for testing
+        $app['config']->set('cache.default', 'array');
+    }
+
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
